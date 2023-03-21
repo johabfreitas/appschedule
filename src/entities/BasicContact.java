7e1f1530145878439cb31fd7247eed37952549e6
Johab <freitasjohab@gmail.com>
2023-03-21 18:05:13 -0300

    created basic contact class

---
+++ src/entities/BasicContact.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class BasicContact {
+
+	private String name;
+	private String telephone;
+
+	public BasicContact() {
+
+	}
+
+	public BasicContact(String name, String telephone) {
+
+		this.name = name;
+		this.telephone = telephone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	public void getData() {
+		
+	}
+
+}
