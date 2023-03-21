15206d347bd4cc872330f5fa312e36d618983e57
Johab <freitasjohab@gmail.com>
2023-03-21 17:57:21 -0300

    First commit - created schedule class

---
+++ README.md
@@ -0,0 +1,1 @@
+# Application developed in Java
---
+++ src/entities/Schedule.java
@@ -0,0 +1,81 @@
+package entities;
+
+public class Schedule {
+
+	private String name;
+	private int position;
+	private String email;
+	private String keyword;
+	
+	public Schedule() {		
+	}
+
+	public Schedule(String name, int position, String email, String keyword) {
+		this.name = name;
+		this.position = position;
+		this.email = email;
+		this.keyword = keyword;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	public void findName(String nome) {
+		
+	}
+	
+	public void findPosition(int position) {
+		
+	}
+	
+	public void findEmail(String email) {
+		
+	}
+	
+	public void findGeneral(String keyword) {
+		
+	}
+	
+	public void findAll() {
+		
+	}
+	
+	public void getQuantity() {
+		
+	}
+	
+	public void insert() {
+		
+	}
+	
+
+}
